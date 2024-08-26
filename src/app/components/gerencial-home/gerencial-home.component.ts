b88c9d454908b3fcf136ca094a23eb11fc363e9f
Vitor Hugo Moreira Souza <vitor.mor2015@gmail.com>
2024-08-26 18:28:44 -0300

    Criação da home do gerencial

--- src/app/components/gerencial-home/gerencial-home.component.ts
+++ src/app/components/gerencial-home/gerencial-home.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-gerencial-home',
   standalone: true,
-  imports: [],
   templateUrl: './gerencial-home.component.html',
-  styleUrl: './gerencial-home.component.scss'
+  styleUrls: ['./gerencial-home.component.scss'],
+  imports: [CommonModule]
 })
 export class GerencialHomeComponent {
 
+  constructor(private router: Router) {}
+
+  logout(): void {
+    sessionStorage.clear(); // Limpa o sessionStorage
+    this.router.navigate(['/login']); // Redireciona para a página de login
+  }
 }
