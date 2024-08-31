c40df83097467847ddfafd5c8b683ed5af17d58c
Vitor Hugo Moreira Souza <vitor.mor2015@gmail.com>
2024-08-31 14:39:48 -0300

    Criação da home do caixa e da home do vendas

--- src/app/components/caixa-home/caixa-home.component.ts
+++ src/app/components/caixa-home/caixa-home.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-caixa-home',
   standalone: true,
-  imports: [],
   templateUrl: './caixa-home.component.html',
-  styleUrl: './caixa-home.component.scss'
+  styleUrls: ['./caixa-home.component.scss']
 })
 export class CaixaHomeComponent {
 
+  constructor(private router: Router) {}
+
+  logout(): void {
+    sessionStorage.clear();
+    this.router.navigate(['/login']);
+  }
+
+  navigateTo(route: string): void {
+    this.router.navigate([`/${route}`]);
+  }
 }
