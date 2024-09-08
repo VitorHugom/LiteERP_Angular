f4b12d0f6ee3cdaaaedc1ec91122a461da22716d
Vitor Hugo Moreira Souza <vitor.mor2015@gmail.com>
2024-09-08 12:29:54 -0300

    Adição do telefone no signup

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -23,27 +23,29 @@ export class SignupComponent {
     signupData = {
         username: '',
         email: '',
+        telefone: '',  // Adicionando o campo telefone
         password: '',
         confirmPassword: '',
         categoriaId: null as number | null
     };
-    
 
     categorias: Categoria[] = [
         { id: 1, nome: 'Vendas' },
         { id: 2, nome: 'Gerencial' },
         { id: 3, nome: 'Caixa' }
     ];
-    isModalOpen: boolean = false
+
+    isModalOpen: boolean = false;
     message: string = '';  // Propriedade para armazenar a mensagem de feedback
 
     constructor(private signupService: SignupService, private router: Router) {}
 
     onSubmit(): void {
-        // Cria o objeto de requisição com categoria_id como número
+        // Cria o objeto de requisição com categoria_id e telefone
         const requestData = {
             nomeUsuario: this.signupData.username,
             email: this.signupData.email,
+            telefone: this.signupData.telefone,  // Inclui o telefone
             senha: this.signupData.password,
             categoria_id: this.signupData.categoriaId
         };
@@ -56,35 +58,36 @@ export class SignupComponent {
                 this.resetForm();
 
                 // Mostra mensagem de sucesso
-                this.isModalOpen = true
+                this.isModalOpen = true;
                 this.message = 'Dados enviados, aguardando liberação.';
             },
             error: (error: HttpErrorResponse) => {
                 console.error('Erro ao registrar usuário:', error);
 
                 // A lógica aqui será executada se ocorrer um erro no envio ou na resposta
-                this.isModalOpen = true
+                this.isModalOpen = true;
                 this.message = 'Erro ao enviar dados. Tente novamente.';
             }
         });
     }
 
-    closeModal(){
-      this.isModalOpen = false
-      this.router.navigate(['/login']);
+    closeModal() {
+        this.isModalOpen = false;
+        this.router.navigate(['/login']);
     }
-
 
     resetForm(): void {
         this.signupData = {
             username: '',
             email: '',
+            telefone: '',  // Reseta o telefone
             password: '',
             confirmPassword: '',
             categoriaId: null
         };
     }
+
     navigateToLogin(): void {
         this.router.navigate(['/login']);
-      }
+    }
 }
