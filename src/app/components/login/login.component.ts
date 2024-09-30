3a94eacc311a9ec7aafae0be36a0de4e89001eb1
skywiliancode <codewilian852@gmail.com>
2024-09-30 19:21:23 -0300

    Tratando exeções e modificando o login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,70 +1,86 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import { LoginService } from '../../services/login.service';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule,ReactiveFormsModule],
   providers: [LoginService]
 })
 export class LoginComponent {
-  username: string = ''
-  password: string = ''
+
+  profileForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(3)])
+  })
+
   errorMessage: string = ''
-  status: boolean = false
+  isError: boolean = false
+
+
 
   constructor(private loginService: LoginService, private router: Router) {}
 
+
   onSubmit(): void {
-    const credentials = {
-      nomeUsuario: this.username,
-      senha: this.password
-    };
 
-    this.loginService.login(credentials).subscribe({
-      next: (response) => {
-        sessionStorage.setItem('auth-token', response.token);
-        sessionStorage.setItem('username', response.nome);
+    if(this.profileForm.valid){
 
-        const token = response.token;
-        const decodedToken = this.decodeToken(token);
-        const role = decodedToken?.role;
+      const {username, password} = this.profileForm.value
 
-        if (role) {
-          sessionStorage.setItem('user-role', role);
-          console.log("Role extraída: " + role);
+      const credentials = {
+        nomeUsuario: String(username),
+        senha: String(password)
+      };
 
-          // Redireciona o usuário com base na role
-          switch (role) {
-            case 'ROLE_GERENCIAL':
-              this.router.navigate(['/gerencial-home']);
-              break;
-            case 'ROLE_VENDAS':
-              this.router.navigate(['/vendas-home']);
-              break;
-            case 'ROLE_CAIXA':
-              this.router.navigate(['/caixa-home']);
-              break;
-            default:
-              this.errorMessage = 'Role não reconhecida.';
-              break;
+      this.loginService.login(credentials).subscribe({
+        next: (response) => {
+          sessionStorage.setItem('auth-token', response.token);
+          sessionStorage.setItem('username', response.nome);
+
+          const token = response.token;
+          const decodedToken = this.decodeToken(token);
+          const role = decodedToken?.role;
+
+          if (role) {
+            sessionStorage.setItem('user-role', role);
+            console.log("Role extraída: " + role);
+
+            // Redireciona o usuário com base na role
+            switch (role) {
+              case 'ROLE_GERENCIAL':
+                this.router.navigate(['/gerencial-home']);
+                break;
+              case 'ROLE_VENDAS':
+                this.router.navigate(['/vendas-home']);
+                break;
+              case 'ROLE_CAIXA':
+                this.router.navigate(['/caixa-home']);
+                break;
+              default:
+                this.errorMessage = 'Role não reconhecida.';
+                break;
+            }
+          } else {
+            console.error('Role não encontrada no token.');
+            this.errorMessage = 'Erro ao autenticar. Role não encontrada.';
           }
-        } else {
-          console.error('Role não encontrada no token.');
-          this.errorMessage = 'Erro ao autenticar. Role não encontrada.';
+        },
+        error: (error) => {
+          console.error('Erro de login:', error);
+          this.errorMessage = 'Usuário ou senha incorretos';
+          this.isError = true
         }
-      },
-      error: (error) => {
-        console.error('Erro de login:', error);
-        this.errorMessage = 'Usuário ou senha incorretos';
-        this.status = true
-      }
-    });
+      });
+    }
+
+
   }
 
   private decodeToken(token: string): any {
