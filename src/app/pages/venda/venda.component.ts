35f90f888ad3c9391bb2382e1e0288d491f563c8
skywiliancode <codewilian852@gmail.com>
2024-10-13 15:25:00 -0300

    Criando o componente venda para colocar na pasta  pages

---
+++ src/app/pages/venda/venda.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-venda',
+  standalone: true,
+  imports: [],
+  templateUrl: './venda.component.html',
+  styleUrl: './venda.component.scss'
+})
+export class VendaComponent {
+
+  constructor(private router: Router) { }
+
+  sair(): void {
+    sessionStorage.clear();
+    this.router.navigate(['/login']);
+  }
+
+  novoPedido(): void {
+    this.router.navigate(['/novo-pedido-vendas']);
+  }
+
+  abrirPedidos(): void {
+    this.router.navigate(['/pedidos-busca']);
+  }
+
+  abrirOrcamentos(): void {
+    this.router.navigate(['/orcamentos']);
+  }
+
+  abrirClientes(): void {
+    this.router.navigate(['clientes-busca']);
+  }
+}
