82d6cabb71aea830f6f0911874644b1f35e67f7b
Ana Flavia <anaflaviamoreirasouzaa@gmail.com>
2025-06-08 13:22:19 -0300

    Back end local

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    // apiUrl: 'http://localhost:8080'
-    apiUrl: 'https://api.lite-erp-enterprise.com' // Substitua pelo IP/URL do backend na rede local
+    apiUrl: 'http://localhost:8080'
+    // apiUrl: 'https://api.lite-erp-enterprise.com' // Substitua pelo IP/URL do backend na rede local
   };
   
