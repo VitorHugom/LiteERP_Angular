cb59827c59a1fc1c67551f7bd664fce3410bb872
Vitor Hugo Moreira Souza <vitormoreira@pactosolucoes.com.br>
2025-10-01 08:12:34 -0300

    Ajuste enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8080'  // Valor para desenvolvimento local
-    // No Docker, este valor será substituído por __API_URL_PLACEHOLDER__ durante o build
-    // e depois pela variável de ambiente API_URL no runtime
-  };
+  production: false,
+  apiUrl: 'http://localhost:8080'
+};
   
